73f94c7f3f5c4980bb10dff48a9d1593f63d2d01
Pandas720 <alvaradu6230@student.faytechcc.edu>
2025-06-16 21:29:10 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_AlvaradoUbaldo.py
@@ -0,0 +1,13 @@
+#Ubaldo Alvarado
+#June 16, 2025
+#P1LAB1
+#Creating Python Program using IDLE and gathering user input and displaying results.
+
+
+print("Enter your first name:", end=" ")
+fName=input()
+
+print("Enter your last name:", end=" ")
+lName=input()
+
+print("Hello,", fName, lName, "! Welcome to CTI-110")
